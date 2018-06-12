ab3cc8246298a5067c6c0c1b9181e8df0428ed85
ANIPLEX <i@cherry.moe>
2018-06-12 16:43:41 +0800

    sss

---
+++ cc/A2.py
@@ -0,0 +1,11 @@
+class Peopel():
+    sum = 0
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        print(self.name)
+
+    def get_age(self):
+        print(self.age)
